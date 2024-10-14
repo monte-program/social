73ab4a37c18429dc8c59c2bbf1f09046480556fd
Alexey Dorofeyev <alexeydorofeyev@MacBook-Air.local>
2024-10-14 18:57:10 +0300

    add files

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,51 @@
+import style from './Menu.module.css'
+
+
+
+
+
+
+
+function Menu(){
+    return(
+        <div>
+
+        <a href="" className={style.link}>Главная</a>
+        <a href="">Контакты</a>
+        <a href="">О нас</a>
+
+
+        </div>
+
+
+
+
+    )
+}
+
+
+
+export default Menu 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
